bef7e3ea894f293f43cf091e9577fb262db107b7
rahul <rahullosharam@gmail.com>
2025-07-10 19:08:34 +0530

    Add files via upload

---
+++ 1-7-25/maxelem.py
@@ -0,0 +1,11 @@
+a=[1,2,6,7,3,4]
+first=second=a[0]
+for i in range(len(a)):
+    if (a[i] > first):
+        second=first
+        first=a[i]
+    elif(a[i] > second and a[i]< first):
+        second    
+print(second)        
+    
+    
