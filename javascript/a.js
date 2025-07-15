3012fbbe095254a08e5d697933f62191365778c8
rahul <rahullosharam@gmail.com>
2025-07-15 22:41:49 +0530

    Add files via upload

---
+++ javascript/a.js
@@ -0,0 +1,38 @@
+alert("welcome to js mr rahul");
+// console.log("hi rahull.....");
+// function rahul(name){
+//   console.log("hello friends, I am". name);
+// }
+// rahul("Rahul")
+// #operators
+// let a=5;
+// let b=2;
+// console.log("a + b =",a+b)
+// console.log("a - b =",a-b)
+// console.log("a ** b =",a**b)
+// console.log("a % b =",a%b)function rahul(name){
+// console.log("a ^ b =",a^b)
+
+// assignment oprators
+// let a=5;
+// console.log(a+=10);
+// console.log(a-=2);
+
+// let a=10,b="10",c=5;
+// console.log("a==b :",a==b);
+// console.log("a===b :",a===b);
+
+//ternary operator
+/*let age=prompt("enter your age:");
+let r= age>18? console.log("adult"):console.log("not adult");*/
+
+for( let i=1;i<=100;i++){
+  console.log("hello rahul");
+}
+console.log("loop has ended");
+  
+
+let f = ["apple","banana","mango"];
+for (let a of f){
+  console.log(a);
+}
